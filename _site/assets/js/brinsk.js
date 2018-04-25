6b50099f202c778fa88a996c706ef98d2a03cf68
akobrinsky <shmaryeh@gmail.com>
2018-04-25 09:27:28 -0400

    do

--- _site/assets/js/brinsk.js
+++ _site/assets/js/brinsk.js
@@ -12,37 +12,17 @@
   $().ready(function() {
 // Default options
 var options = {
-   animationDuration: 0.5, // in seconds
-   filter: 'all', // Initial filter
-   callbacks: { 
-      onFilteringStart: function() { },
-      onFilteringEnd: function() { },
-      onShufflingStart: function() { },
-      onShufflingEnd: function() { },
-      onSortingStart: function() { },
-      onSortingEnd: function() { }
-   },
-   controlsSelector: '', // Selector for custom controls
-   delay: 0, // Transition delay in ms
-   delayMode: 'progressive', // 'progressive' or 'alternate'
-   easing: 'ease-out',
-   filterOutCss: { // Filtering out animation
+   filterOutCss: {
       opacity: 0,
       transform: 'scale(0.5)'
    },
-   filterInCss: { // Filtering in animation
+   filterInCss: {
       opacity: 1,
-      transform: 'scale(1)'
+      transform: 'scale(0.5)'
    },
-   layout: 'packed', // See layouts
-   multifilterLogicalOperator: 'or',
-   selector: '.filtr-container',
-   setupControls: true // Should be false if controlsSelector is set 
+   layout: 'packed', 
 } 
-// You can override any of these options and then call...
-var filterizd = $('.filtr-container').filterizr(options);
-// If you have already instantiated your Filterizr then call...
-filterizd.filterizr('setOptions', options);
+var filterizd = $('.post-filter').filterizr(options);
 });
 
   $().ready(function() {
