807505f5fb22880e5024e1fa29a5f43f2d87f8f6
akobrinsky <shmaryeh@gmail.com>
2018-04-30 17:00:15 -0400

    dodo

--- _site/assets/js/brinsk.js
+++ _site/assets/js/brinsk.js
@@ -12,17 +12,36 @@
   $().ready(function() {
 // Default options
 var options = {
-   filterOutCss: {
+   animationDuration: 0.5, // in seconds
+   filter: 'all', // Initial filter
+   callbacks: { 
+      onFilteringStart: function() { },
+      onFilteringEnd: function() { },
+      onShufflingStart: function() { },
+      onShufflingEnd: function() { },
+      onSortingStart: function() { },
+      onSortingEnd: function() { }
+   },
+   controlsSelector: '', // Selector for custom controls
+   delay: 0, // Transition delay in ms
+   delayMode: 'progressive', // 'progressive' or 'alternate'
+   easing: 'ease-out',
+   filterOutCss: { // Filtering out animation
       opacity: 0,
       transform: 'scale(0.5)'
    },
-   filterInCss: {
+   filterInCss: { // Filtering in animation
       opacity: 1,
-      transform: 'scale(0.5)'
+      transform: 'scale(1)'
    },
-   layout: 'packed', 
+   layout: 'packed', // See layouts
+   multifilterLogicalOperator: 'or',
+   setupControls: true // Should be false if controlsSelector is set 
 } 
+// You can override any of these options and then call...
 var filterizd = $('.post-filter').filterizr(options);
+// If you have already instantiated your Filterizr then call...
+filterizd.filterizr('setOptions', options);
 });
 
   $().ready(function() {
